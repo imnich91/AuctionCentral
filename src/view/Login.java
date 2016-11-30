d30460aed72465ecff1d8d0fb47ce2372d140e3f
Colin <Colin@10.16.8.213>
2016-11-30 15:11:59 -0800

    Login Class 	button change

--- src/view/Login.java
+++ src/view/Login.java
@@ -9,6 +9,7 @@ package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
@@ -23,12 +24,12 @@ public class Login extends JPanel {
 	/**
 	 * Used to give button access to whole class.
 	 */
-	private JMenuItem myLogin;
+	private JButton myLogin;
 	
 	/**
 	 * Used to give button access to whole class.
 	 */
-	private JMenuItem myExit;
+	private JButton myExit;
 	
 	/**
 	 * Used to make panel.
@@ -45,7 +46,7 @@ public class Login extends JPanel {
 	 * Used to make exit button.
 	 */
 	private void makeButtonExit() {
-		myExit = new JMenuItem("Exit");
+		myExit = new JButton("Exit");
 	    myExit.addActionListener(new ActionListener() {
 	    	@Override
 	        public void actionPerformed(final ActionEvent theEvent) {
@@ -59,7 +60,7 @@ public class Login extends JPanel {
 	 * Used to make login button.
 	 */
 	private void makeButtonLogin() {
-		   myLogin = new JMenuItem("Login");
+		   myLogin = new JButton("Login");
 	       myLogin.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(final ActionEvent theEvent) {
