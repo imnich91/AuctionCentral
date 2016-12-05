72f66de7f0c8cd0c46621b2dac4efbd1f9d00d0c
Colin <Colin@10.16.8.213>
2016-12-05 14:12:19 -0800

    Bidder Panel 	place bid button

--- src/view/BidderPanel.java
+++ src/view/BidderPanel.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -21,7 +22,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import model.Auction;
+import model.Bid;
 import model.Calendar;
+import model.Item;
 import model.User;
 
 /**
@@ -33,7 +36,7 @@ public class BidderPanel extends JPanel {
 	
 	/**
 	 * Used to keep track of data.
-	 */
+	 * */
 	private static final long serialVersionUID = 1L;
 	
 	/**
@@ -45,6 +48,11 @@ public class BidderPanel extends JPanel {
 	 * Used to tell users information.
 	 */
 	private JPanel myTextPanel;
+	
+	/**
+	 * Used to gain access to the frame.
+	 */
+	private JFrame myFrame;
 	
 	/**
 	 * Used for formatting.
@@ -87,12 +95,24 @@ public class BidderPanel extends JPanel {
 	private User myUser;
 	
 	/**
+	 * USed to keep track of non-profit
+	 */
+	private Auction myNonProfit;
+	
+	/**
+	 * Used to keep track of item getting bid on.
+	 */
+	private Item myItem;
+	
+	/**
 	 * Used to build the JPanel.
-	 * 
 	 * @param theFrame the frame everything is loaded into
 	 */
 	public BidderPanel(final JFrame theFrame, final Calendar theCalendar) {
 		setLayout(new BorderLayout());
+		myFrame = theFrame;
+		myItem = null;
+		myNonProfit = null;
 		myCalendar = theCalendar;
 		myUser = null;
 		myAuctions = (ArrayList<Auction>)myCalendar.getAuctions();
@@ -162,16 +182,102 @@ public class BidderPanel extends JPanel {
 					list.add(a.getName());
 				}
 				String[] choices = (list).toArray(new String[0]);
-			    String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
+				String input ="";
+			    input = (String) JOptionPane.showInputDialog(null, "Choose now...",
 			        "Non-Profit List", JOptionPane.QUESTION_MESSAGE, null,
 			        choices, choices[0]);
 			    
-			    //ask user for item they want to bid on.
-			    
-			        
+			    String item ="";
+			    item = askForItem(input);
+			    findItem(item);
+			    if(alreadyBid()) {
+			    	askForBid();
+			    } else {
+			    	popUpBidTwice();
+			    }
 			}
+
 		});
 		myButtons.add(myPlaceBid);
+	}
+	
+	private void popUpBidTwice() {
+		JOptionPane.showMessageDialog(myFrame, "You have already Bid on this item once"
+				, "Bid Error", JOptionPane.ERROR_MESSAGE);
+	}
+	
+	/**
+	 * Makes a pop up telling the user that they
+	 * already have a bid on this item.
+	 * 
+	 * @return if user is trying to make two bids
+	 */
+	private boolean alreadyBid() {
+		boolean flag = true;
+		Collection<Bid> Bids = myItem.getBunchObids();
+		for(Bid a: Bids) {
+			if((a.getBidder()).equals(myUser)) {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	/**
+	 * Used to ask user how much they want to bid
+	 * on an item
+	 * @param theItem item to bid on
+	 */
+	private void askForBid() {
+		Double.parseDouble(
+	            JOptionPane.showInputDialog(this, "Type in grade:"));	
+	}
+	
+	/**
+	 * Used to find item person wants to bid
+	 * on.
+	 * 
+	 * @param theItem the name of the item
+	 */
+	private void findItem(String theItem) {
+		if(!(myNonProfit).equals(null)) {
+			Collection<Item> auctions = (myNonProfit.getInventory());
+			for(Item a : auctions) {
+				if(theItem == a.getItemName()){
+					myItem = a;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Used to make a pop up that will
+	 * ask the user what item the want to bid on.
+	 * 
+	 * @param theNonProfit Non-profit name
+	 * @return item to bid on
+	 */
+	private String askForItem(String theNonProfit) {
+		String biddingItem = "";
+		if (myCalendar != null) {
+			List<Auction> auctions =(myCalendar.getAuctions());
+			for(Auction a : auctions) {
+				if(theNonProfit == a.getName()){
+					myNonProfit = a;
+				}
+			}
+			List<String> item = new ArrayList<String>();
+			Collection<Item> inventory = myNonProfit.getInventory();
+			for(Item b : inventory) {
+				item.add(b.getItemName());
+			}
+			String[] items = (item).toArray(new String[0]);
+			biddingItem = (String) JOptionPane.showInputDialog(null, "Choose now...",
+					"Item List", JOptionPane.QUESTION_MESSAGE, null,
+					items, items[0]);
+		}
+		return biddingItem;
+		
 	}
 	
 	/**
